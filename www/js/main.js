48b609227756b84d5e95991b23e26ee2aa398570
Scott Holdsworth <scott.holdsworth@hotmail.com>
2019-02-05 17:32:15 +0000

    Initial commit

---
+++ www/js/main.js
@@ -0,0 +1,7 @@
+// this is phonegap specific document.addEventListener("deviceready", setup, false);
+
+
+function setup()
+{ /   navigator.notification.alert(     'You are the winner!',  message     alertDismissed,  callback     'Game Over',  title     'Done'  buttonName   );
+
+}
