959f637a47610eb6cda9b96686cdb67611a0ec07
Scott Holdsworth <scott.holdsworth@hotmail.com>
2019-02-12 21:12:03 +0000

    Page 1 + 2

--- www/js/main.js
+++ www/js/main.js
@@ -1,7 +1,20 @@
 // this is phonegap specific document.addEventListener("deviceready", setup, false);
 
 
-function setup()
-{ /   navigator.notification.alert(     'You are the winner!',  message     alertDismissed,  callback     'Game Over',  title     'Done'  buttonName   );
+//function setup(){ /   navigator.notification.alert(     'You are the winner!',  message     alertDismissed,  callback     'Game Over',  title     'Done'  buttonName   );}
 
+showAlert: function (message, title){
+  if (navigator.notification){
+    navigator.notification.alert(message, null, title, 'ok');
+  } else {
+    alert(title ? (title + ": " + message) : message);
+  }
+},
+
+initialize: function(){
+  var self = this;
+  this.store = new MemoryStore(function(){
+    self.showAlert('Store initialized', 'Info');
+  });
+  $('.search-key').on('keyup', $.proxy(this.findByName, this));
 }
